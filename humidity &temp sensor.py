453012b15134c3ee14b87170fad2a1eae7dc5d8b
CATHERINE KABURA <103778014+Kaburaa@users.noreply.github.com>
2022-08-05 15:21:15 +0300

    Add files via upload
    digital temperature nd humidity sensor

---
+++ humidity &temp sensor.py
@@ -0,0 +1,12 @@
+from machine import Pin
+from time import sleep
+import dht
+
+sensor = dht.DHT11(Pin(2))
+
+while (True):
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    print(":- Temperature: {}C Humidity: {:.0f}% ".format(temp, hum))
+    sleep(2)
